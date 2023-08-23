66b200d203f6ac365fbc13d6eda72e19adde12ae
christian <chris@siebold.cloud>
2023-08-23 12:04:59 +0200

    Update

--- backend/api/getTemplate.php
+++ backend/api/getTemplate.php
@@ -1,15 +1,25 @@
 <?php
 require_once '../config.php';
 
-// Hier können Sie den Template-String aus Ihrer Datenbank abrufen.
-// Zum Beispiel:
-$stmt = $pdo->prepare("SELECT content FROM templates WHERE id = ?");
-$stmt->execute([$_GET['id']]);
-$template = $stmt->fetch();
+// Überprüfen, ob die TemplateID im GET-Parameter vorhanden ist
+if(isset($_GET['id'])) {
+    $templateId = $_GET['id'];  // Die neue TemplateID aus dem GET-Parameter holen
 
-if($template) {
-    echo json_encode(['status' => 'success', 'template' => $template['content']]);
+    // Hier können Sie den Template-String und den templateName aus Ihrer Datenbank abrufen.
+    $stmt = $pdo->prepare("SELECT content, templateName FROM templates WHERE templateId = ?");
+    $stmt->execute([$templateId]);
+    $template = $stmt->fetch();
+
+    if($template) {
+        echo json_encode([
+            'status' => 'success', 
+            'template' => $template['content'],
+            'templateName' => $template['templateName']  // Fügen Sie den templateName hinzu
+        ]);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Template not found.']);
+    }
 } else {
-    echo json_encode(['status' => 'error', 'message' => 'Template not found.']);
+    echo json_encode(['status' => 'error', 'message' => 'Invalid or missing TemplateID.']);
 }
 ?>
--- backend/api/saveTemplate.php
+++ backend/api/saveTemplate.php
@@ -3,19 +3,26 @@ require_once '../config.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(isset($data->template)) {
+if(isset($data->template) && isset($data->templateName)) {
     $template = $data->template;
+    $templateName = $data->templateName;  // Extrahieren Sie den templateName
+    
+    // Generieren Sie den Zeitstempel
+    $timestamp = time();
+    
+    // Erstellen Sie die neue TemplateID
+    $templateId = $templateName . '_' . $timestamp;
     
     // Konvertieren Sie das Template-Array in einen String
     $templateString = json_encode($template);
     
-    // Speichern Sie den Template-String in Ihrer Datenbank
-    $stmt = $pdo->prepare("INSERT INTO templates (content) VALUES (?)");
-    $stmt->execute([$templateString]);
-    
-    $templateId = $pdo->lastInsertId();
-    
-    echo json_encode(['status' => 'success', 'templateId' => $templateId]);
+    // Speichern Sie den Template-String, den templateName und die neue TemplateID in Ihrer Datenbank
+    $stmt = $pdo->prepare("INSERT INTO templates (content, templateName, templateId) VALUES (?, ?, ?)");
+    if ($stmt->execute([$templateString, $templateName, $templateId])) {
+        echo json_encode(['status' => 'success', 'templateId' => $templateId]);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Fehler: ' . implode(", ", $stmt->errorInfo())]);
+    }
 } else {
     echo json_encode(['status' => 'error', 'message' => 'Invalid data.']);
 }
